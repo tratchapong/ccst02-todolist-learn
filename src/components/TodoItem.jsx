a61020fbd26b4da98685f7863db771b4a0e2b840
tratchapong <tratchapong@gmail.com>
2024-06-12 09:28:22 +0700

    update Job complete via checkbox

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 function TodoItem({ job, reload, editId, setEditId }) {
   const [inputTitle, setInputTitle] = useState(job.todo)
+  const [jobDone, setJobDone] = useState(job.completed)
 
   const hdlDelete = async () => {
     if (!confirm("Confirm delete ?")) {
@@ -17,7 +18,7 @@ function TodoItem({ job, reload, editId, setEditId }) {
   };
 
   const hdlUpdate = async () => {
-    const body = { ...job, todo: inputTitle }
+    const body = { ...job, todo: inputTitle, completed: jobDone }
     try {
       await axios.put(`http://localhost:8000/jobs/${job.id}`,body)
       reload()
@@ -35,13 +36,16 @@ function TodoItem({ job, reload, editId, setEditId }) {
     <div className="todo-item">
       {job.id === editId ? (
         <>
+          <input type="checkbox" checked={jobDone} onChange={e=>setJobDone(e.target.checked)} />
           <input type="text" value={inputTitle} onChange={e=>setInputTitle(e.target.value)} />
           <button onClick={hdlUpdate}>Save</button>
           <button onClick={hdlCancel}>cancel</button>
         </>
       ) : (
         <>
-          <input type="text" disabled value={job.todo} />
+          <input type="text" disabled value={job.todo} 
+            style={{color: job.completed? 'green' : ''}}
+          />
           <button onClick={() => setEditId(job.id)}>Edit</button>
           <button onClick={hdlDelete}>Delete</button>
         </>
